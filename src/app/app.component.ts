9db26a44465c2f8334d3656f71ea10f7a7dbc31c
amkumar072 <amkumar072@gmail.com>
2021-06-25 09:43:57 +0100

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as grapesjs from 'grapesjs';
+import 'grapesjs-preset-newsletter';
+import 'grapesjs-preset-webpage';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit() {
+    const editor = grapesjs.init({
+      container: '#gjs',
+      plugins: ['gjs-preset-newsletter', 'grapesjs-preset-webpage'],
+      pluginsOpts: {
+        'gjs-preset-newsletter': {
+          modalTitleImport: 'Import template',
+          // ... other options
+        },
+        'grapesjs-preset-webpage': {
+
+        }
+      }
+    });
+  }
 }
